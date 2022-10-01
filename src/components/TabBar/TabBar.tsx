743ee8cd3ec604fc6f449153bd6158506a16d28f
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-09-30 21:40:21 -0600

    tab bar tab

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -2,14 +2,24 @@ import React, { useMemo } from 'react';
 import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 import { theme } from 'theme';
 
-import type { Props } from './TabBar.types';
+import type { Props, TabProps } from './TabBar.types';
 
 const tabBarStyles = StyleSheet.create({
+  base: {},
+});
+
+const tabStyles = StyleSheet.create({
   base: {},
 });
 
 function TabBar(props: Props) {
   return null;
 }
+
+function Tab(props: TabProps) {
+  return null;
+}
+
+TabBar.Tab = Tab;
 
 export default TabBar;
