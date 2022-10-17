fe06a7492150548d73c7e054e2083f82c5b55ab4
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-10-16 17:05:05 -0700

    add children props

--- src/components/Heading/Heading.types.ts
+++ src/components/Heading/Heading.types.ts
@@ -1,5 +1,8 @@
+import type { TextProps } from 'react-native';
+
 export interface Props {
   align?: 'left' | 'center' | 'right';
+  children?: TextProps['children'];
   variant: 'display1' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   weight?: 'light' | 'normal' | 'bold';
 }
--- src/components/Text/Text.types.ts
+++ src/components/Text/Text.types.ts
@@ -1,5 +1,8 @@
+import type { TextProps } from 'react-native';
+
 export interface Props {
   align?: 'left' | 'center' | 'right';
+  children?: TextProps['children'];
   variant: 'body1' | 'body2' | 'caption';
   weight?: 'light' | 'normal' | 'bold';
 }
