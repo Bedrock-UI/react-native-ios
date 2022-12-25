2a619da57a2cdef8d285677aa0f0ac01cb6244f1
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-12-25 12:29:09 -0700

    max width horizontal scroll

--- src/components/HorizontalScroll/HorizontalScroll.tsx
+++ src/components/HorizontalScroll/HorizontalScroll.tsx
@@ -1,9 +1,22 @@
-import { ScrollView } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 
 import type { Props } from './HorizontalScroll.types';
 
-function HorizontalScroll(props: Props) {
-  return <ScrollView {...props} horizontal={true} showsHorizontalScrollIndicator={false} />;
+const styles = StyleSheet.create({
+  root: {
+    width: '100%',
+  },
+});
+
+function HorizontalScroll({ style, ...props }: Props) {
+  return (
+    <ScrollView
+      {...props}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      style={[style, styles.root]}
+    />
+  );
 }
 
 export default HorizontalScroll;
